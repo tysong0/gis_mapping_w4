3107ccb24fd46b2c6836d92e23b7eaa64e9b5326
tysong0 <165327100+tysong0@users.noreply.github.com>
2024-04-29 21:58:33 -0400

    First ver., finished all the basic functions

---
+++ js/quakecount.js
@@ -0,0 +1,262 @@
+[
+  {
+    "abb": "AL",
+    "name": "Alabama",
+    "count": 409
+  },
+  {
+    "abb": "AK",
+    "name": "Alaska",
+    "count": 6177
+  },
+  {
+    "abb": "AZ",
+    "name": "Arizona",
+    "count": 1206
+  },
+  {
+    "abb": "AR",
+    "name": "Arkansas",
+    "count": 1542
+  },
+  {
+    "abb": "CA",
+    "name": "California",
+    "count": 73673
+  },
+  {
+    "abb": "CO",
+    "name": "Colorado",
+    "count": 2072
+  },
+  {
+    "abb": "CT",
+    "name": "Connecticut",
+    "count": 632
+  },
+  {
+    "abb": "DE",
+    "name": "Delaware",
+    "count": 117
+  },
+  {
+    "abb": "DC",
+    "name": "District of Columbia",
+    "count": 21
+  },
+  {
+    "abb": "FL",
+    "name": "Florida",
+    "count": 136
+  },
+  {
+    "abb": "GA",
+    "name": "Georgia",
+    "count": 524
+  },
+  {
+    "abb": "HI",
+    "name": "Hawaii",
+    "count": 6305
+  },
+  {
+    "abb": "ID",
+    "name": "Idaho",
+    "count": 2718
+  },
+  {
+    "abb": "IL",
+    "name": "Illinois",
+    "count": 3297
+  },
+  {
+    "abb": "IN",
+    "name": "Indiana",
+    "count": 1337
+  },
+  {
+    "abb": "IA",
+    "name": "Iowa",
+    "count": 392
+  },
+  {
+    "abb": "KS",
+    "name": "Kansas",
+    "count": 380
+  },
+  {
+    "abb": "KY",
+    "name": "Kentucky",
+    "count": 1709
+  },
+  {
+    "abb": "LA",
+    "name": "Louisiana",
+    "count": 96
+  },
+  {
+    "abb": "ME",
+    "name": "Maine",
+    "count": 2336
+  },
+  {
+    "abb": "MD",
+    "name": "Maryland",
+    "count": 294
+  },
+  {
+    "abb": "MA",
+    "name": "Massachusetts",
+    "count": 1494
+  },
+  {
+    "abb": "MI",
+    "name": "Michigan",
+    "count": 470
+  },
+  {
+    "abb": "MN",
+    "name": "Minnesota",
+    "count": 271
+  },
+  {
+    "abb": "MS",
+    "name": "Mississippi",
+    "count": 374
+  },
+  {
+    "abb": "MO",
+    "name": "Missouri",
+    "count": 2495
+  },
+  {
+    "abb": "MT",
+    "name": "Montana",
+    "count": 5911
+  },
+  {
+    "abb": "NE",
+    "name": "Nebraska",
+    "count": 314
+  },
+  {
+    "abb": "NV",
+    "name": "Nevada",
+    "count": 3803
+  },
+  {
+    "abb": "NH",
+    "name": "New Hampshire",
+    "count": 1262
+  },
+  {
+    "abb": "NJ",
+    "name": "New Jersey",
+    "count": 1053
+  },
+  {
+    "abb": "NM",
+    "name": "New Mexico",
+    "count": 746
+  },
+  {
+    "abb": "NY",
+    "name": "New York",
+    "count": 3415
+  },
+  {
+    "abb": "NC",
+    "name": "North Carolina",
+    "count": 975
+  },
+  {
+    "abb": "ND",
+    "name": "North Dakota",
+    "count": 94
+  },
+  {
+    "abb": "OH",
+    "name": "Ohio",
+    "count": 1188
+  },
+  {
+    "abb": "OK",
+    "name": "Oklahoma",
+    "count": 674
+  },
+  {
+    "abb": "OR",
+    "name": "Oregon",
+    "count": 2660
+  },
+  {
+    "abb": "PA",
+    "name": "Pennsylvania",
+    "count": 1379
+  },
+  {
+    "abb": "RI",
+    "name": "Rhode Island",
+    "count": 184
+  },
+  {
+    "abb": "SC",
+    "name": "South Carolina",
+    "count": 1087
+  },
+  {
+    "abb": "SD",
+    "name": "South Dakota",
+    "count": 454
+  },
+  {
+    "abb": "TN",
+    "name": "Tennessee",
+    "count": 1856
+  },
+  {
+    "abb": "TX",
+    "name": "Texas",
+    "count": 585
+  },
+  {
+    "abb": "UT",
+    "name": "Utah",
+    "count": 2101
+  },
+  {
+    "abb": "VT",
+    "name": "Vermont",
+    "count": 1203
+  },
+  {
+    "abb": "VA",
+    "name": "Virginia",
+    "count": 890
+  },
+  {
+    "abb": "WA",
+    "name": "Washington",
+    "count": 9850
+  },
+  {
+    "abb": "WV",
+    "name": "West Virginia",
+    "count": 298
+  },
+  {
+    "abb": "WI",
+    "name": "Wisconsin",
+    "count": 387
+  },
+  {
+    "abb": "WY",
+    "name": "Wyoming",
+    "count": 1885
+  },
+  {
+    "abb": "PR",
+    "name": "Puerto Rico",
+    "count": 528
+  }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var STARTING_CENTER = [-73.26651, 40.79090]
 var ZOOM_SW = [-74.87235, 40.19620]
 var ZOOM_NE = [-71.66068, 41.38031]
+var hoveredPolygonId = null;
 
 // Set max zoom out bounds to the US
 const bounds = [
@@ -18,66 +19,164 @@ const map = new mapboxgl.Map({
     maxBounds: bounds // Set the map's geographical boundaries.
 })
 
-map.on('load', function () {
-    map.resize();
-});
-
-//iterate
-earthquakes.forEach(function(earthquakerecord) {
-   
-    var magnitude = earthquakerecord.mag; 
-    var colour;
-    var utcDate = `${earthquakerecord.time}`;  // ISO-8601 formatted date returned from the original dataset
-    var localDate = new Date(utcDate);  // Display in local time
-
-    //set the color of the markers
-    // If the earthquake is more significant than M5.0
-    if (magnitude >= 5.0) {
-      colour = "red";
-    
-    // If the earthquake is between M4.0 and 5.0
-    } else if (magnitude >= 4.0 ) {
-      colour = "orange";
-    
-    // If the earthquake is more minor than M4.0
-    } else {
-      colour = "yellow";
-    }
-
-    //create popup incl. quake info and stick them on the markers
-    const popup = new mapboxgl.Popup({ 
-        offset: 40,
-        anchor: 'bottom'
-    }).setText(
-        `There was a M${earthquakerecord.mag} earthquake, happened at ${localDate}.`
-    );
-
-    //create the markers
-    new mapboxgl.Marker({ 
-    color: colour 
-    })
-    .setLngLat([earthquakerecord.longitude, earthquakerecord.latitude])
-    .setPopup(popup)
-    .addTo(map);
-})
+// add a navigation control
+map.addControl(new mapboxgl.NavigationControl());
 
 // add a scale to the map
 map.addControl(new mapboxgl.ScaleControl());
 
-/*
-     *  When a user clicks the button, `fitBounds()` zooms and pans
-     *  the viewport to contain a bounding box that surrounds Kenya.
-     *  The [lng, lat] pairs are the southwestern and northeastern
-     *  corners of the specified geographical bounds.
-     */
-document.getElementById('fit').addEventListener('click', () => {
-    map.fitBounds([
-        ZOOM_SW, // southwestern corner of the bounds
-        ZOOM_NE // northeastern corner of the bounds
-    ]);
+map.on('load', () => {
+
+    map.resize();
+
+    map.addSource('states', {
+        "type": "geojson",
+        "data": "data/us_states.geojson"
+    });
+
+    // add a fill layer using the PLUTO data
+    map.addLayer({
+        'id': 'states-fill',
+        'type': 'fill',
+        'source': 'states',
+        'layout': {},
+        'paint': {
+            'fill-color': { // use an expression for data-driven styling
+                property: 'count',
+                stops: [[0, '#fff'], [13000, '#f00']]
+            },
+            'fill-opacity': [
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                1.5,
+                0.5
+            ]
+        }
+    });
+
+    //add a line layer for the states
+    map.addLayer({
+        'id': 'states-line',
+        'type': 'line',
+        'source': 'states',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': 2.5
+        }
+    }, 'path-pedestrian-label');
+
+    // When the user moves their mouse over the state-fill layer, we'll update the feature state for the feature under the mouse.
+    map.on('mousemove', 'states-fill', (e) => {
+        if (e.features.length > 0) {
+            if (hoveredPolygonId !== null) {
+                map.setFeatureState(
+                    { source: 'states', id: hoveredPolygonId },
+                    { hover: false }
+                );
+            }
+            hoveredPolygonId = e.features[0].id;
+            map.setFeatureState(
+                { source: 'states', id: hoveredPolygonId },
+                { hover: true }
+            );
+        }
+    });
+
+    // When the mouse leaves the state-fill layer, update the feature state of the
+    // previously hovered feature.
+    map.on('mouseleave', 'states-fill', () => {
+        if (hoveredPolygonId !== null) {
+            map.setFeatureState(
+                { source: 'states', id: hoveredPolygonId },
+                { hover: false }
+            );
+        }
+        hoveredPolygonId = null;
+    });
+
+    //iterate
+    earthquakes.forEach(function (earthquakerecord) {
+
+        var magnitude = earthquakerecord.mag;
+        var colour;
+        var utcDate = `${earthquakerecord.time}`;  // ISO-8601 formatted date returned from the original dataset
+        var localDate = moment(utcDate).format('LL'); // Display in local time, using the moment.js
+        var coordinates = [earthquakerecord.longitude, earthquakerecord.latitude];
+
+        //set the color of the markers
+        // If the earthquake is more significant than M5.0
+        if (magnitude >= 5.0) {
+            colour = "red";
+
+            // If the earthquake is between M4.0 and 5.0
+        } else if (magnitude >= 4.0) {
+            colour = "orange";
+
+            // If the earthquake is more minor than M4.0
+        } else {
+            colour = "yellow";
+        }
+
+        //create popup incl. quake info and stick them on the markers
+        const popup = new mapboxgl.Popup({
+            offset: 40,
+            anchor: 'bottom',
+            closeButton: false,
+            closeOnClick: false
+        }).setText(
+            `There was a M${earthquakerecord.mag} earthquake, happened at ${localDate} local time.`
+        );
+
+        //create the markers
+        const marker = new mapboxgl.Marker({
+            color: colour
+        })
+            .setLngLat([earthquakerecord.longitude, earthquakerecord.latitude])
+            .setPopup(popup)
+            .addTo(map);
+        const markerDiv = marker.getElement();
+        markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
+        markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
+    })
+
+    /*
+         *  When a user clicks the button, `fitBounds()` zooms and pans
+         *  the viewport to contain a bounding box that surrounds Kenya.
+         *  The [lng, lat] pairs are the southwestern and northeastern
+         *  corners of the specified geographical bounds.
+         */
+    document.getElementById('fit').addEventListener('click', () => {
+        map.fitBounds([
+            ZOOM_SW, // southwestern corner of the bounds
+            ZOOM_NE // northeastern corner of the bounds
+        ]);
+    });
+
+    // When a click event occurs on a feature in the states layer,
+    // open a popup at the location of the click, with description
+    // HTML from the click event's properties.
+    map.on('click', 'states-fill', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.name)
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when
+    // the mouse is over the states layer.
+    map.on('mouseenter', 'states-fill', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change the cursor back to a pointer
+    // when it leaves the states layer.
+    map.on('mouseleave', 'states-fill', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+
+
+
 });
-
-
-
-
-
